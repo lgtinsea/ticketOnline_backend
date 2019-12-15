b22fc5b49d289ad971605d10e5a549a43fb5f5dc
lgtinsea <1753029@tongji.edu.cn>
2019-12-15 14:23:22 +0800

    fix some small bugs for the swagger

--- adminSystem/src/main/java/com/tjCourse/softwareEngineering/adminSystemApplication.java
+++ adminSystem/src/main/java/com/tjCourse/softwareEngineering/adminSystemApplication.java
@@ -1,11 +1,19 @@
 package com.tjCourse.softwareEngineering;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.tjCourse.softwareEngineering.dao")
+@EnableCaching
+@EnableSwagger2
+@EnableTransactionManagement
 public class adminSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(adminSystemApplication.class,args);
--- userSystem/src/main/java/com/tjCourse/softwareEngineering/controller/testGateway.java
+++ userSystem/src/main/java/com/tjCourse/softwareEngineering/controller/testGateway.java
@@ -1,9 +1,11 @@
 package com.tjCourse.softwareEngineering.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api", produces = "application/json;charset=utf-8")
 public class testGateway {
     @GetMapping("/hello")
     public String hello(){
